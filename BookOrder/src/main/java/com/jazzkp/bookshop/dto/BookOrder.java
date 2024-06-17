7d61c63071dd1cd0fcb38cd6c7315c93372f7b34
Kacper Pach <s27112@pjwstk.edu.pl>
2024-06-17 22:41:10 +0200

    feign fix

--- BookOrder/src/main/java/com/jazzkp/bookshop/dto/BookOrder.java
+++ BookOrder/src/main/java/com/jazzkp/bookshop/dto/BookOrder.java
@@ -2,17 +2,14 @@ package com.jazzkp.bookshop.dto;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
 
 @Data
 @Entity
-public class BookOrder {
+public class BookOrderDta {
     @Id
-    @UuidGenerator
     private UUID bookId;
     private Integer orderAmount;
 }
